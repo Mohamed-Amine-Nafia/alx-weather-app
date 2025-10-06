edb62a04fbbd71580467cbab3e099d385caa286b
Mohamed Amine Nafia <m.amine.nafia@gmail.com>
2025-10-06 23:17:58 +0200

    add an active state the suggested cities

--- src/components/SuggestedCity.jsx
+++ src/components/SuggestedCity.jsx
@@ -21,7 +21,7 @@ function SuggestedCity() {
           <span
             onClick={() => handleClick(city)}
             key={city}
-            className=" bg-black/10 block w-fit my-2 backdrop-blur-lg text-white py-2 px-3 cursor-pointer hover:scale-105 duration-200 ease-in-out"
+            className=" bg-black/10 block w-fit my-2 backdrop-blur-lg text-white py-2 px-3 cursor-pointer hover:scale-105 duration-200 ease-in-out active:bg-white/80 active:text-black"
           >
             {city}
           </span>
